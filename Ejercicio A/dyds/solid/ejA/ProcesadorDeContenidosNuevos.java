acfe28910b6a0b99f948d40fa6a729bf382b1fd5
Emanuel Tiradani <ematiradani@gmail.com>
2022-03-28 00:56:28 -0300

    Cambiar el arreglo por una lista, ciruja

--- Ejercicio A/dyds/solid/ejA/ProcesadorDeContenidosNuevos.java
+++ Ejercicio A/dyds/solid/ejA/ProcesadorDeContenidosNuevos.java
@@ -1,13 +1,23 @@
 package dyds.solid.ejA;
 
 public class ProcesadorDeContenidosNuevos {
-	RepositorioDummy repo = new RepositorioDummy();
+	Poster poster = new Poster();
+	RepositorioDummy repo = new RepositorioDummy(); //Sacar el repo a una interfaz?
 
-	public void procesar(Contenido contenidoNuevo) {
+	public void procesar(Contenido contenidoNuevo){
+		if(contenidoNuevo.validar() && repo.grabar(contenidoNuevo)){
+			poster.post(contenidoNuevo);
+		}
+	}
+	public RepositorioDummy getRepo() { return repo; }
+
+	/*public void procesar(Contenido contenidoNuevo) {
 		if (contenidoNuevo.validar() && repo.grabar(contenidoNuevo)) {
 			ManejadorDeChulogram.getInstance().postearContenido(contenidoNuevo);
 		}
 	}
 
 	public RepositorioDummy getRepo() { return repo; }
+
+	 */
 }
