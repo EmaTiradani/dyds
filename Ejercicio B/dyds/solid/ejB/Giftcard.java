afbcc4bb0f578a0655df5ca208b165ade53fdc17
Emanuel Tiradani <ematiradani@gmail.com>
2022-04-03 23:32:27 -0300

    Empece a cambiar lo de la giftcard(singleton) y me dio paja

--- Ejercicio B/dyds/solid/ejB/Giftcard.java
+++ Ejercicio B/dyds/solid/ejB/Giftcard.java
@@ -3,13 +3,21 @@ package dyds.solid.ejB;
 import java.util.Random;
 
 public class Giftcard implements Premio{
+
+    private static Giftcard instance;
     int codigo;
     Usuario beneficiario;
 
+    private Giftcard(){
+    }
 
-    public Giftcard(int ultimoCodigo, Usuario beneficiario) {
-        this.codigo = (new Random().nextInt(9000) + 1000)*10000 + ultimoCodigo;
-        this.beneficiario = beneficiario;
+    public static Giftcard getInstance(int ultimoCodigo, Usuario beneficiario) {
+        if(instance == null){
+            instance = new Giftcard();
+            this.codigo = (new Random().nextInt(9000) + 1000)*10000 + ultimoCodigo;
+            this.beneficiario = beneficiario;
+        }
+        return instance;
     }
 
     public int darCodigo() {
