44d04a90be86fa52c813a8acb7171615a23d4494
Emanuel Tiradani <ematiradani@gmail.com>
2022-04-05 21:12:07 -0300

    Termine de sacar comments y borre una class

--- Ejercicio C/dyds/solid/ejC/LogicaPersonajes.java
+++ Ejercicio C/dyds/solid/ejC/LogicaPersonajes.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LogicaPersonajes {
-	//private FiltrosPersonaje filtro = new FiltrosPersonaje();
+
 	private List<Personaje> personajes =  new ArrayList<Personaje>();
 
 	public List<Personaje> getListaFiltrada(String nombre, Profesion profesion, int nivel, Filtro filtro){
@@ -14,21 +14,4 @@ public class LogicaPersonajes {
 	}
 
 	public List<Personaje> getPersonajes() { return personajes; }
-/*
-	public List<Personaje> getPersonajesConNombre(String nombre) {
-		return filtro.filtroContieneNombre(personajes, nombre);
-	}
-	
-	public List<Personaje> getPersonajesConNivelMayorA(int nivel) {
-
-		return filtro.filtroSuperaNivel(personajes, nivel);
-	}
-
-	public List<Personaje> getPersonajesConProfesion(Profesion profesion) {
-
-		return filtro.filtradoPorProfesion(personajes, profesion);
-	}
-*/
-
-
 }
